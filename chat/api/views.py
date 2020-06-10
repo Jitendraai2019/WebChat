d1c23ac319c5abaa5f3f306fe4c18b056645727e
Jitendra <jitendraai2019@gmail.com>
2020-06-10 17:41:10 +0530

    modifications in the search api

--- chat/api/views.py
+++ chat/api/views.py
@@ -2,8 +2,14 @@ from rest_framework import status
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
 
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+
+from django.core.serializers import serialize
+
 from chat.api.serializers import RoomSerializer
 from chat.models import Room
+
 
 
 @api_view(['GET', ])
@@ -17,3 +23,19 @@ def api_search_room(request, room_name):
     if request.method == 'GET':
         serializer = RoomSerializer(room_name)
         return Response(serializer.data)
+ 
+
+# @api_view(['GET', ])
+# def api_search_room(request, room_name):
+#     ''''''
+#     try:
+#         room_names = Room.objects.filter(room_name__contains=room_name)
+#     except Room.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         # serializer = RoomSerializer(room_name)
+#         # return Response(serializer.data)
+#         # serialized = serialize('json', room_name)
+#         serialized = json.dumps(list(room_names), cls=DjangoJSONEncoder)
+#         return Response(serialized)
