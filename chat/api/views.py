054651961d2706f374999c85f0f8450259866b15
Jitendra <jitendraai2019@gmail.com>
2020-06-11 20:10:14 +0530

    Correct search contains with function based API.

--- chat/api/views.py
+++ chat/api/views.py
@@ -12,30 +12,30 @@ from chat.models import Room
 
 
 
-@api_view(['GET', ])
-def api_search_room(request, room_name):
-    ''''''
-    try:
-        room_name = Room.objects.get(room_name=room_name)
-    except Room.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = RoomSerializer(room_name)
-        return Response(serializer.data)
- 
-
 # @api_view(['GET', ])
 # def api_search_room(request, room_name):
 #     ''''''
 #     try:
-#         room_names = Room.objects.filter(room_name__contains=room_name)
+#         room_name = Room.objects.get(room_name=room_name)
 #     except Room.DoesNotExist:
 #         return Response(status=status.HTTP_404_NOT_FOUND)
     
 #     if request.method == 'GET':
-#         # serializer = RoomSerializer(room_name)
-#         # return Response(serializer.data)
-#         # serialized = serialize('json', room_name)
-#         serialized = json.dumps(list(room_names), cls=DjangoJSONEncoder)
-#         return Response(serialized)
+#         serializer = RoomSerializer(room_name)
+#         return Response(serializer.data)
+ 
+
+@api_view(['GET', ])
+def api_search_room(request, room_name):
+    ''''''
+    try:
+        room_names = Room.objects.filter(room_name__contains=room_name)
+    except Room.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = RoomSerializer(room_names, many=True)
+        # return Response(serializer.data)
+        # serialized = serialize('json', room_name)
+        # serialized = json.dumps(list(room_names), cls=DjangoJSONEncoder)
+        return Response(serializer.data)
